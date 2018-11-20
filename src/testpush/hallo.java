108cb045c000815905331594d92a0533cace4e76
u434596 <u434596@R-114.cadlab.tu-berlin.de>
2018-11-20 14:35:06 +0100

    das ist ein test

---
+++ src/testpush/hallo.java
@@ -0,0 +1,21 @@
+package testpush;
+
+public class hallo {
+
+	public static void main(String[] args) {
+		int a = 9;
+		int b = 5;
+		
+		int sum = a + b;
+		int diff = a - b;
+		
+		System.out.println(a + " + " + b + " = " + sum);
+		System.out.println(a + " - " + b + " = " + diff);
+		
+		System.out.println(a + b + " ist die Lösung");
+		System.out.println("Lösung: " + a + b);
+		System.out.println("Lösung: " + (a + b));
+		
+	}
+
+}
